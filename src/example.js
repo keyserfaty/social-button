a43481cf3c7000ef39ff5fdba644c4cd7a859caa
Karen <karen@iamat.com>
2016-07-02 10:19:22 -0300

    update example

--- src/example.js
+++ src/example.js
@@ -7,6 +7,11 @@
     background: '#aaa',
   });
 
+  var button2 = socialButton.create({
+    strategy: 'facebook',
+    container: '.container',
+  });
+
   button.onclick = function() {
     console.log('click');
   };
