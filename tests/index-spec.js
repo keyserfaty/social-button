4cef16b3d5b8a3b3ee812b95b7479846f5b71d1d
Karen <karen@iamat.com>
2016-07-03 16:24:42 -0300

    add tests

--- tests/index-spec.js
+++ tests/index-spec.js
@@ -1,7 +1,9 @@
 var jsdom = require('mocha-jsdom');
 var expect = require('chai').expect;
 
-var appendChilds = function appendChildren(container) {
+
+
+var appendChildren = function appendChildren(container) {
   return function appendToContainer() {
     return Array.prototype.map.call(arguments, function (elem) {
       container.appendChild(elem);
@@ -17,19 +19,78 @@ var setAttributes = function(container, attrs) {
   return container;
 };
 
-var createElementWithClass = function (tag, attrs) {
-  var node = doc.createElement(tag);
-  setAttributes(node, attrs);
-  return node;
-};
 
-describe('mocha tests', function () {
+
+describe('appendChildren', function () {
 
   jsdom();
 
-  it('has document', function () {
-    var div = document.createElement('div');
-    expect(div.nodeName).eql('DIV');
+  it('should be a function', function () {
+    /**
+     * Create basic elements for testing
+     */
+    var doc = document;
+
+    var container = doc.createElement('div');
+    container.setAttribute('id', 'container');
+
+    var container = appendChildren(container);
+    expect(container).to.be.a('function');
+  });
+
+  it('should append three children', function () {
+    /**
+     * Create basic elements for testing
+     */
+    var doc = document;
+
+    var container = doc.createElement('div');
+    container.setAttribute('id', 'container');
+
+    var child1 =  doc.createElement('div');
+    var child2 =  doc.createElement('div');
+    var child3 =  doc.createElement('div');
+
+    var containerForChildren = appendChildren(container);
+    containerForChildren(child1, child2, child3);
+
+    expect(container.childElementCount).to.eqls(3);
+  });
+
+  it('should have three attributes', function () {
+    /**
+     * Create basic elements for testing
+     */
+    var doc = document;
+
+    var container = doc.createElement('div');
+    var containerWithAttrs = setAttributes(container, {
+      'class': 'test',
+      'id': 'test',
+      'thing': 'test',
+    });
+
+    expect(container.attributes.length).to.eqls(3);
+  });
+
+  it('should create a div element with two attributes', function () {
+    /**
+     * Create basic elements for testing
+     */
+
+    var createElementWithClass = function (tag, attrs) {
+      var node = document.createElement(tag);
+      setAttributes(node, attrs);
+      return node;
+    };
+
+    var element = createElementWithClass('div', {
+      'class': 'test',
+      'id': 'test',
+    });
+    
+    expect(element.nodeName).eql('DIV')
+    expect(element.attributes.length).to.eqls(2);
   });
 
 });
