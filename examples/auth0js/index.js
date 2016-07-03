6e969f1e3c2d598c7a5c10a7c834b8ffd8f2e15a
Karen <karen@iamat.com>
2016-07-03 15:20:55 -0300

    add auth0js example

--- examples/auth0js/index.js
+++ examples/auth0js/index.js
@@ -1,18 +1,44 @@
-(function(socialButton) {
+(function(socialButton, Auth0) {
+  // Define a strategy
+  var strategy = 'github';
+
+  // Register a new Auth0 instance
+  var auth0 = new Auth0({
+    domain:       'keyserfaty.auth0.com',
+    clientID:     'LwOkdeAnWYgifq9b4nnRHZ1vMsef63Sz',
+    callbackURL:  'http://localhost:8080',
+    callbackOnLocationHash: true
+  });
+
+  // Build a button
   var button = socialButton.create({
-    strategy: 'twitter',
-    container: '.container',
-    label: 'Twitter magic login',
-    icon: 'http://simpleicon.com/wp-content/uploads/twitter.png',
-    background: '#aaa',
+    strategy: strategy,
+    container: '#container',
   });
 
-  var button2 = socialButton.create({
-    strategy: 'facebook',
-    container: '.container',
-  });
-
+  // Handle onclick event
   button.onclick = function() {
-    console.log('click');
+    auth0.login({
+      connection: strategy
+    });
   };
-}(socialButton))
+
+  // Once logged in..
+
+  // Parse hash from the URL
+  var result = auth0.parseHash(window.location.hash);
+
+  // Use result.id_token to call your rest api
+
+  if (result && result.id_token) {
+    auth0.getProfile(result.id_token, function (err, profile) {
+      alert('hello ' + profile.name);
+    });
+    // If offline_access was a requested scope
+    // You can grab the result.refresh_token here
+
+  } else if (result && result.error) {
+    alert('error: ' + result.error);
+  }
+
+}(socialButton));
